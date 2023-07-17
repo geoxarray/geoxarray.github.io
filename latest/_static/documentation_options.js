c4dd236d4059055df34cc584a9523058811a4f31
djhoese <djhoese@users.noreply.github.com>
2023-07-17 01:47:27 +0000

    Deploy geoxarray.github.io website for SHA:ef43e04b0b8667d009537657563abf9c2c0499ed (Ref: refs/heads/main)

--- latest/_static/documentation_options.js
+++ latest/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.dev97+g7a21467',
+    VERSION: '0.1.dev110+gef43e04',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
